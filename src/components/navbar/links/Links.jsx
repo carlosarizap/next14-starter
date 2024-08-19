a0ecdc05f9ab10d43f514aa3bdf0ba84da07a3c6
Carlos Ariza <carlosariza7459@gmail.com>
2024-08-19 11:21:24 -0400

    diseño

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -48,7 +48,14 @@ const Links = ({session}) => {
           <NavLink item={{ title: "Login", path: "/login" }} />
         )}
       </div>
-        <button className={styles.menyButton} onClick={() => setOpen(prev => !prev)}>Menu</button>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,23 @@
+import styles from './postCard.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const PostCard = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/27351031/pexels-photo-27351031/free-photo-of-mar-ciudad-vacaciones-agua.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' fill className={styles.img}/>
+                </div>
+                <span className={styles.date}>01.01.2024</span>
+            </div>
+            <div className={styles.bottom}>
+                <h1 className={styles.title}>Title</h1>
+                <p className={styles.desc}>Desc</p>
+                <Link className={styles.link} href={"/blog/post"}>READ MORE</Link>
+            </div>
+        </div>
+    )
+}
+
+export default PostCard
